f4fec995c853e3ef4248071882c95e4e05d454f1
Lokesh <Lokesh@github>
2025-02-18 17:06:19 +0530

    IMPLEMENTED OTHER SCREENS
    - Implemented added screens in the drawer.

--- app/src/main/java/com/bookdel/app/Navigation/NavGraph.kt
+++ app/src/main/java/com/bookdel/app/Navigation/NavGraph.kt
@@ -5,8 +5,11 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.bookdel.app.Screens.AboutUsScreen
+import com.bookdel.app.Screens.AddressScreen
 import com.bookdel.app.Screens.CartScreen
 import com.bookdel.app.Screens.MainScreen
+import com.bookdel.app.Screens.PaymentScreen
 
 @Composable
 fun SetupNavGraph(
@@ -21,5 +24,19 @@ fun SetupNavGraph(
         composable<NavigationScreens.CartScreen> {
             CartScreen(innerPadding = innerPadding)
         }
+
+        composable<NavigationScreens.AddressScreen> {
+            AddressScreen(innerPadding = innerPadding)
+        }
+
+        composable<NavigationScreens.PaymentScreen> {
+            PaymentScreen(innerPadding = innerPadding)
+
+        }
+        composable<NavigationScreens.AboutUsScreen> {
+            AboutUsScreen(innerPadding = innerPadding)
+
+        }
+
     }
 }
