bbfd88838259d52310422f342b75ee0cd8ae59ae
programmer <programmer@github.com>
2025-02-19 23:56:04 +0530

    Added image field book data class, since it was missing.

--- app/src/main/java/com/bookdel/app/Data/Book.kt
+++ app/src/main/java/com/bookdel/app/Data/Book.kt
@@ -1,9 +1,11 @@
 package com.bookdel.app.Data
 
+import androidx.compose.ui.graphics.vector.ImageVector
 import java.util.Date
 
 data class Book(
     val name: String? = null,
+    val image: ImageVector,
     val author: String? = null,
     val publishedDate: Date? = null,
     val cost: Int? = null,
